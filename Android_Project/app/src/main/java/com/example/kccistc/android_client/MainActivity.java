7409efa87e8da2277ad7f6dd38592e65a6fa5ea6
BYG <33047026+qodydrjs@users.noreply.github.com>
2018-09-13 17:28:26 +0900

    안드로이드
    ㅇ

--- Android_Project/app/src/main/java/com/example/kccistc/android_client/MainActivity.java
+++ Android_Project/app/src/main/java/com/example/kccistc/android_client/MainActivity.java
@@ -5,14 +5,20 @@ import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,113 +27,91 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String default_ip = "192.168.1.61";
-    private int default_Port = 5001;
-    //Member
-    private TextView et;
-    private EditText ip, port;
-    private Button btn_start,btn_reset,btn_send;
     private BottomNavigationView bottomNavigationView;
     private ViewPager viewPager;
 
-    Client socket2;
+
+    private TabLayout mTabLayout;
+
+    private int[] mTabsIcons={
+      R.drawable.ic_filter_black_24dp,
+      R.drawable.ic_videocam_black_24dp,
+      R.drawable.ic_brightness_5_black_24dp
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager = findViewById(R.id.ViewPager1);
-        viewPager.setAdapter(getSupportFragmentManager());
-        et = findViewById(R.id.tx_log);
-        btn_start = findViewById(R.id.btn_start);
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-        //bottomNavigationView 리스너 설정
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.navigation_1:
-                        return true;
-                    case R.id.navigation_2:
-                        return true;
-                    case R.id.navigation_3:
-                        return  true;
-                }
-                return false;
+        // 뷰페이퍼 셋업
+        CustomViewPager viewPager = (CustomViewPager) findViewById(R.id.ViewPager1);
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+
+        if(viewPager != null){
+            viewPager.setPagingEnabled(false);
+            viewPager.setAdapter(pagerAdapter);
+        }
+
+        mTabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        if (mTabLayout != null) {
+            mTabLayout.setupWithViewPager(viewPager);
+
+            for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+                TabLayout.Tab tab = mTabLayout.getTabAt(i);
+                if (tab != null)
+                    tab.setCustomView(pagerAdapter.getTabView(i));
             }
-        });
-
-        ///메세지 SEND 핸들러..
-        final Handler getMessageHandler = new Handler(){
-            @Override
-            public void handleMessage(Message msg) {
-                et.setText("Message 받음"+msg.getData().get("msg"));
-            }
-        };
-
-        btn_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ip = findViewById(R.id.ed_ip);
-                port = findViewById(R.id.ed_port);
-
-//                Toast t = Toast.makeText(getApplicationContext(),ip.getText().toString() + "" +port.getText().toString(),Toast.LENGTH_SHORT);
-//                t.show();
-                try {
-                    if (ip.getText().toString().length() > 5 && port.getText().toString().length() > 2) {
-                        socket2 = new Client(ip.getText().toString(), Integer.parseInt(port.getText().toString()));
-                    } else {
-                        socket2 = new Client(default_ip, default_Port);
-                    }
-                    if (!socket2.equals(null)) {
-                        setClientCallback(socket2, getMessageHandler);
-                        socket2.connect();
-                    }
-                }catch (Exception e){
-                    try {
-                        socket2.disconnect();
-                    }catch (Exception es){es.printStackTrace();}
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
+            mTabLayout.getTabAt(0).getCustomView().setSelected(true);
+        }
     }
 
-    private void setClientCallback(final Client c_socket,final Handler getMessageHandler){
-        c_socket.setClientCallback(new Client.ClientCallback () {
-            @Override
-            public void onMessage(final String message) {
-                //Main UI 쓰레드에서 처리 해야하므로 생성 다른 쓰레드에서 View 변경시 오류.
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Message msg = getMessageHandler.obtainMessage();
-                        Bundle b = new Bundle();
-                        b.putString("msg",message);
-                        msg.setData(b);
-                        getMessageHandler.sendMessage(msg);
-                    }
-                }).start();
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public final int PAGE_COUNT = 3;
+
+        private final String[] mTabsTitle = {"Recents", "Favorites", "Nearby"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public View getTabView(int position) {
+            // Given you have a custom layout in `res/layout/custom_tab.xml` with a TextView and ImageView
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_tab, null);
+            TextView title = (TextView) view.findViewById(R.id.title);
+            title.setText(mTabsTitle[position]);
+            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+            icon.setImageResource(mTabsIcons[position]);
+            return view;
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch (pos) {
+
+                case 0:
+                    return PageFragment.newInstance(1);
+                case 1:
+                    return PageFragment.newInstance(2);
+                case 2:
+                    return PageFragment.newInstance(3);
+
             }
+            return null;
+        }
 
-            @Override
-            public void onConnect(Socket socket) {
-                c_socket.send("안드로이드에서 접속함");
-                // socket.disconnect();
-            }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
 
-            @Override
-            public void onDisconnect(Socket socket, String message) {
-
-            }
-
-            @Override
-            public void onConnectError(Socket socket, String message) {
-
-            }
-        });
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTabsTitle[position];
+        }
     }
 }
+
+
