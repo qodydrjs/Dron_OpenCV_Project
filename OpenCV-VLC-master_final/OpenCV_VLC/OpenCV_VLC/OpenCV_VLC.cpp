68d836ed29d12df767f4527d43f7412e00b9eec4
BYG <33047026+qodydrjs@users.noreply.github.com>
2018-11-01 00:38:15 +0900

    Update
    드론해상도 수정

--- OpenCV-VLC-master_final/OpenCV_VLC/OpenCV_VLC/OpenCV_VLC.cpp
+++ OpenCV-VLC-master_final/OpenCV_VLC/OpenCV_VLC/OpenCV_VLC.cpp
@@ -8,6 +8,8 @@
 #include <vlc/vlc.h>
 #include <process.h>
 #include <thread>
+#include <cstdio>
+#include <ctime>
 
 #include "opencv2/objdetect.hpp"
 #include "opencv2/videoio.hpp"
@@ -27,9 +29,15 @@ struct ctx
 	HANDLE mutex;
 	uchar*    pixels;
 };
+// define output video resolution
+#define VIDEO_WIDTH     1024
+#define VIDEO_HEIGHT    768
+//#define VIDEO_WIDTH     800
+//#define VIDEO_HEIGHT    600
 
-#define VIDEO_WIDTH     400
-#define VIDEO_HEIGHT    300
+//#define VIDEO_WIDTH     500
+//#define VIDEO_HEIGHT    300
+unsigned WINAPI HandleClnt(void * arg);
 
 #define TEXTSIZE  12000
 #define MAXBUFLEN 256
@@ -37,7 +45,6 @@ struct ctx
 SQLHENV henv = SQL_NULL_HENV;
 SQLHDBC hdbc1 = SQL_NULL_HDBC;
 SQLHSTMT hstmt1 = SQL_NULL_HSTMT;
-
 
 void geterror(SQLHSTMT hstmt)
 {
@@ -74,18 +81,24 @@ void Cleanup()
 	if (henv != SQL_NULL_HENV)
 		SQLFreeHandle(SQL_HANDLE_ENV, henv);
 }
-
+////////////////////
 
 /** Global variables */
 String face_cascade_name;
 CascadeClassifier face_cascade;
+CvHaarClassifierCascade* cascade = 0;
 String window_name = "Face detection";
-
-Mat frame;
-
+//static Mat frame;
+HANDLE hThread;
 mssqlx64 mssql;
-
 char buf[256];
+boolean checkfalg = false;
+HANDLE hMutex;
+//struct ctx* context2;
+////타이머
+std::clock_t start;
+double duration;
+
 
 void *lock(void *data, void**p_pixels)
 {
@@ -107,184 +120,22 @@ void unlock(void *data, void *id, void *const *p_pixels) {
 
 	// get back data structure 
 	struct ctx *ctx = (struct ctx*)data;
-
 	/* VLC just rendered the video, but we can also render stuff */
 	// show rendered image
 	imshow("test", *ctx->image);
-
-	frame = ctx->image->clone();
-
-	std::vector<Rect> faces;
-	Mat frame_gray;
-
-	cv::cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-	equalizeHist(frame_gray, frame_gray);
-
-	boolean checkfalg = false;
-
-	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
-	for (size_t i = 0; i < faces.size(); i++)
-	{
-		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2),
-			0, 0, 360, Scalar(0, 0, 255), 4, 8, 0);
-
-		checkfalg = true;
-	}
-
-
-
-	if (checkfalg) {
-
-		sprintf(buf, "c:/temp/img01.jpg", 0);
-		std::cout << buf << std::endl;
-		imwrite(buf, frame);
-
-		//frame.data
-		RETCODE retcode;
-		// SQLBindParameter variables.
-		SQLLEN cbTextSize, lbytes;
-		// SQLParamData variable.
-		PTR pParmID;
-		// SQLPutData variables.
-		string s = "C:\\temp\\img01.jpg"; // any large image file
-
-		ifstream fin;
-		fin.open(s.c_str(), ios::binary | ios::ate);
-
-		size_t sz = fin.tellg();
-		fin.seekg(0, ios::beg);
-
-		char *ptr = new char[sz];
-
-		fin.read(ptr, sz);
-
-		fin.close();
-
-		SDWORD cbBatch = sz;
-
-		// Allocate the ODBC environment and save handle.
-		retcode = SQLAllocHandle(SQL_HANDLE_ENV, NULL, &henv);
-		if ((retcode != SQL_SUCCESS_WITH_INFO) && (retcode != SQL_SUCCESS))
-		{
-			printf("SQLAllocHandle(Env) Failed\n\n");
-			Cleanup();
-		}
-
-		// Notify ODBC that this is an ODBC 3.0 app.
-		retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3,
-			SQL_IS_INTEGER);
-		if ((retcode != SQL_SUCCESS_WITH_INFO) && (retcode != SQL_SUCCESS))
-		{
-			printf("SQLSetEnvAttr(ODBC version) Failed\n\n");
-			Cleanup();
-		}
-
-		// Allocate ODBC connection handle and connect.
-		retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc1);
-		if ((retcode != SQL_SUCCESS_WITH_INFO) && (retcode != SQL_SUCCESS))
-		{
-			printf("SQLAllocHandle(hdbc1) Failed\n\n");
-			Cleanup();
-		}
-
-		retcode = SQLConnectW(hdbc1, (SQLWCHAR*)L"mssql", SQL_NTSL, (SQLWCHAR*)L"sa", SQL_NTSL, (SQLWCHAR*)L"123qwe", SQL_NTSL);
-
-
-		if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO))
-		{
-			printf("SQLConnect() Failed\n\n");
-			Cleanup();
-		}
-
-		// Allocate statement handle.
-		retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc1, &hstmt1);
-		if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO))
-		{
-			printf("SQLAllocHandle(hstmt1) Failed\n\n");
-			Cleanup();
-		}
-
-		// Set parameters based on total data to send.
-
-		lbytes = (SDWORD)sz;
-		cbTextSize = SQL_LEN_DATA_AT_EXEC(lbytes);
-
-		// Bind the parameter marker.
-
-		SQLSMALLINT   NumParams2, DataType2, DecimalDigits2, Nullable2;
-
-		//x86
-		//SQLUINTEGER   ParamSize2;
-		//x64
-		SQLULEN   ParamSize2;
-		retcode = SQLPrepareA(hstmt1, (SQLCHAR*)"INSERT INTO imageTable3 (name, data) VALUES ('file2', ?)", SQL_NTS);
-
-		retcode = SQLDescribeParam(hstmt1, 1, &DataType2, &ParamSize2, &DecimalDigits2, &Nullable2);
-
-		retcode = SQLBindParameter(hstmt1, 1, SQL_PARAM_INPUT, SQL_C_BINARY,
-			DataType2, ParamSize2, 0, (VOID *)1, 0, &cbTextSize);
-
-		if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO)) {
-			printf("SQLBindParameter Failed\n\n");
-			Cleanup();
-		}
-
-		// Execute the command.
-		retcode = SQLExecDirectA(hstmt1, (UCHAR*)"INSERT INTO imageTable3 (name, data) VALUES ('file2', ?)", SQL_NTS);
-
-		if ((retcode != SQL_SUCCESS) && (retcode != SQL_NEED_DATA) && (retcode != SQL_SUCCESS_WITH_INFO))
-		{
-			printf("SQLExecDirect Failed\n\n");
-			Cleanup();
-		}
-
-		retcode = SQLParamData(hstmt1, &pParmID);
-
-		if (retcode == SQL_NEED_DATA)
-		{
-			while (lbytes > cbBatch)
-			{
-				retcode = SQLPutData(hstmt1, (SQLCHAR*)ptr, cbBatch);
-
-				geterror(hstmt1);
-
-				lbytes -= cbBatch; 
-				ptr += cbBatch;
-			}
-			// Put final batch.
-			retcode = SQLPutData(hstmt1, ptr, lbytes);
-		}
-
-		if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO))
-		{
-			printf("SQLParamData Failed\n\n");
-			Cleanup();
-		}
-
-		// Make final SQLParamData call.
-		retcode = SQLParamData(hstmt1, &pParmID);
-		if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO))
-		{
-			printf("Final SQLParamData Failed\n\n");
-			Cleanup();
-		}
-		checkfalg = false;
-
-		// Clean up.
-		SQLFreeHandle(SQL_HANDLE_STMT, hstmt1);
-		SQLDisconnect(hdbc1);
-		SQLFreeHandle(SQL_HANDLE_DBC, hdbc1);
-		SQLFreeHandle(SQL_HANDLE_ENV, henv);
-		delete[] ptr;
-	}
+	//face_cascade.detectMultiScale(frame_gray, faces, 1.1, 5, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
 	ReleaseMutex(ctx->mutex);
 }
 
 int main()
 {
-		face_cascade_name = "C:\\project\\Dron_OpenCV_Project\\OpenCV-VLC-master_final\\OpenCV_VLC\\OpenCV_VLC\\haarcascade_frontalface_alt.xml";
+
+	///////////타이머 시작
+	start = std::clock();
+	//hMutex = CreateMutex(NULL, FALSE, NULL);
+
+	//face_cascade_name = "D:\\OpenCV-VLC-master\\OpenCV_VLC\\OpenCV_VLC\\haarcascade_frontalface_alt.xml";
+	face_cascade_name = "C:\\project\\Dron_OpenCV_Project\\OpenCV-VLC-master_final\\OpenCV_VLC\\OpenCV_VLC\\haarcascade_frontalface_alt.xml";
 
 	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading face cascade\n"); return -1; };
 
@@ -311,17 +162,20 @@ int main()
 
 	libvlc_media_release(media);
 
-	struct ctx* context = (struct ctx*)malloc(sizeof(*context));
+	struct ctx*context = (struct ctx*)malloc(sizeof(*context));
+
 	context->mutex = CreateMutex(NULL, FALSE, NULL);
 
 	context->image = new Mat(VIDEO_HEIGHT, VIDEO_WIDTH, CV_8UC3);
 	context->pixels = (unsigned char *)context->image->data;
 	// show blank image
+
 	imshow("test", *context->image);
+
+	hThread = (HANDLE)_beginthreadex(NULL, 0, HandleClnt, (void *)context, 0, NULL);
 
 	libvlc_video_set_callbacks(mp, lock, unlock, display, context);
 	libvlc_video_set_format(mp, "RV24", VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_WIDTH * 24 / 8); // pitch = width * BitsPerPixel / 8
-
 
 	int ii = 0;
 	int key = 0;
@@ -333,9 +187,200 @@ int main()
 			libvlc_media_player_play(mp);
 		}
 		float fps = libvlc_media_player_get_fps(mp);
-		printf("fps:%f\r\n", fps);
+		//printf("fps:%f\r\n", fps);
 		key = waitKey(100); // wait 100ms for Esc key
 	}
+
 	libvlc_media_player_stop(mp);
+	return 0;
+}
+
+unsigned WINAPI HandleClnt(void * arg) { // 
+
+	struct ctx *ctx = (struct ctx*)arg;
+
+	while (1)
+	{
+		//cout << "2 cho" << endl;
+		duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+		if (((int)duration == 1))
+		{
+			//checkfalg = true;
+			start = std::clock();
+
+			std::vector<Rect> faces;
+			Mat frame_f = ctx->image->clone();
+			Mat frame_gray;
+			cv::cvtColor(frame_f, frame_gray, COLOR_BGR2GRAY);
+			equalizeHist(frame_gray, frame_gray);
+			face_cascade.detectMultiScale(frame_gray, faces, 1.1, 5, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+			for (size_t i = 0; i < faces.size(); i++)
+			{
+				cv::Point lb(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+				cv::Point tr(faces[i].x, faces[i].y);
+				cv::rectangle(frame_f, lb, tr, cv::Scalar(0, 255, 0), 3, 4, 0);
+
+			}
+			if (faces.size()) {
+
+				sprintf(buf, "c:/temp/img01.jpg", 0);
+				imwrite(buf, frame_f);
+
+				RETCODE retcode;
+				// SQLBindParameter variables.
+				SQLLEN cbTextSize, lbytes;
+				// SQLParamData variable.
+				PTR pParmID;
+				// SQLPutData variables.
+				string s = "C:\\temp\\img01.jpg"; // any large image file
+
+				ifstream fin;
+				fin.open(s.c_str(), ios::binary | ios::ate);
+
+				size_t sz = fin.tellg();
+				fin.seekg(0, ios::beg);
+
+				char *ptr = new char[sz];
+
+				fin.read(ptr, sz);
+
+				fin.close();
+
+				SDWORD cbBatch = sz;
+
+				// Allocate the ODBC environment and save handle.
+				retcode = SQLAllocHandle(SQL_HANDLE_ENV, NULL, &henv);
+				if ((retcode != SQL_SUCCESS_WITH_INFO) && (retcode != SQL_SUCCESS))
+				{
+					printf("SQLAllocHandle(Env) Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+
+				// Notify ODBC that this is an ODBC 3.0 app.
+				retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3,
+					SQL_IS_INTEGER);
+				if ((retcode != SQL_SUCCESS_WITH_INFO) && (retcode != SQL_SUCCESS))
+				{
+					printf("SQLSetEnvAttr(ODBC version) Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+
+				// Allocate ODBC connection handle and connect.
+				retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc1);
+				if ((retcode != SQL_SUCCESS_WITH_INFO) && (retcode != SQL_SUCCESS))
+				{
+					printf("SQLAllocHandle(hdbc1) Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+
+				retcode = SQLConnectW(hdbc1, (SQLWCHAR*)L"mssql", SQL_NTSL, (SQLWCHAR*)L"sa", SQL_NTSL, (SQLWCHAR*)L"123qwe", SQL_NTSL);
+
+
+				if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO))
+				{
+					printf("SQLConnect() Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+
+				// Allocate statement handle.
+				retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc1, &hstmt1);
+				if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO))
+				{
+					printf("SQLAllocHandle(hstmt1) Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+
+				// Set parameters based on total data to send.
+
+				lbytes = (SDWORD)sz;
+				cbTextSize = SQL_LEN_DATA_AT_EXEC(lbytes);
+
+				// Bind the parameter marker.
+
+				SQLSMALLINT   NumParams2, DataType2, DecimalDigits2, Nullable2;
+
+				//x86
+				//SQLUINTEGER   ParamSize2;
+				//x64
+				SQLULEN   ParamSize2;
+				retcode = SQLPrepareA(hstmt1, (SQLCHAR*)"INSERT INTO imageTable3 (name, data) VALUES ('file2', ?)", SQL_NTS);
+
+				retcode = SQLDescribeParam(hstmt1, 1, &DataType2, &ParamSize2, &DecimalDigits2, &Nullable2);
+
+				retcode = SQLBindParameter(hstmt1, 1, SQL_PARAM_INPUT, SQL_C_BINARY,
+					DataType2, ParamSize2, 0, (VOID *)1, 0, &cbTextSize);
+
+				if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO)) {
+					printf("SQLBindParameter Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+
+				// Execute the command.
+				retcode = SQLExecDirectA(hstmt1, (UCHAR*)"INSERT INTO imageTable3 (name, data) VALUES ('file2', ?)", SQL_NTS);
+
+				if ((retcode != SQL_SUCCESS) && (retcode != SQL_NEED_DATA) && (retcode != SQL_SUCCESS_WITH_INFO))
+				{
+					printf("SQLExecDirect Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+
+				// Check to see if NEED_DATA; if yes, use SQLPutData.
+
+				retcode = SQLParamData(hstmt1, &pParmID);
+
+				if (retcode == SQL_NEED_DATA)
+				{
+					while (lbytes > cbBatch)
+					{
+						retcode = SQLPutData(hstmt1, (SQLCHAR*)ptr, cbBatch);
+
+						geterror(hstmt1);
+
+						lbytes -= cbBatch;
+						ptr += cbBatch;
+					}
+					// Put final batch.
+					retcode = SQLPutData(hstmt1, ptr, lbytes);
+				}
+
+				if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO))
+				{
+					printf("SQLParamData Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+
+				// Make final SQLParamData call.
+				retcode = SQLParamData(hstmt1, &pParmID);
+				if ((retcode != SQL_SUCCESS) && (retcode != SQL_SUCCESS_WITH_INFO))
+				{
+					printf("Final SQLParamData Failed\n\n");
+					Cleanup();
+					//return(9);
+				}
+				checkfalg = false;
+
+				// Clean up.
+				SQLFreeHandle(SQL_HANDLE_STMT, hstmt1);
+				SQLDisconnect(hdbc1);
+				SQLFreeHandle(SQL_HANDLE_DBC, hdbc1);
+				SQLFreeHandle(SQL_HANDLE_ENV, henv);
+
+				delete[] ptr;
+
+			}
+
+
+		}
+	}
+
 	return 0;
 }
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -26,6 +26,7 @@ File file;
 void ErrorHandling(const char *message);
 unsigned WINAPI HandleClnt(void *arg);
 void SendMsg(char *msg, int len);
+unsigned WINAPI SendImage(void *arg);
 
 int clntCnt = 0;
 SOCKET clntSocks[MAX_CLNT];
@@ -65,7 +66,7 @@ int main(int argc, char *argv[])
 	SOCKADDR_IN servAddr, clntAddr;
 
 	int clntAdrSz;
-	HANDLE hThread;
+	HANDLE hThread, hThread_Image;
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) //소켓 라이브러리 초기화
 		ErrorHandling("WSAStartup() error!");
@@ -88,6 +89,9 @@ int main(int argc, char *argv[])
 	if (listen(hServSock, 5) == SOCKET_ERROR) //listen 함수호출을 통해서 생성한 소켓을 서버 소켓으로 완성
 		ErrorHandling("listen() error");
 
+
+	hThread_Image = (HANDLE)_beginthreadex(NULL, 0, SendImage, NULL, 0, NULL);
+
 	while (1)
 	{
 		clntAdrSz = sizeof(clntAddr);
@@ -101,8 +105,17 @@ int main(int argc, char *argv[])
 		clntSocks[clntCnt++] = hClntSock;
 		ReleaseMutex(hMutex);
 
+		//hThread = (std::thread)_beginthreadex(NULL, 0, HandleClnt, (void *)&hClntSock, 0, NULL);
 		hThread = (HANDLE)_beginthreadex(NULL, 0, HandleClnt, (void *)&hClntSock, 0, NULL);
+
 		printf("Connected client IP: %s \n", inet_ntoa(clntAddr.sin_addr));
+
+		//send(hClntSock, message, sizeof(message), 0); //send함수 호출을 통해서 연결된 클라이언트에 데이터를 전송
+
+		//std::thread t1(send_msg1, &hClntSock);
+		//std::thread t2(send_msg, &hClntSock);
+
+		//t1.join();
 	}
 	closesocket(hServSock);
 	WSACleanup(); //프로그램 종료 전에 초기화한 소켓 라이브러리 해제
@@ -125,157 +138,6 @@ unsigned WINAPI HandleClnt(void * arg) { //
 
 	while ((strLen = recv(hClntSock, msg, sizeof(msg), 0)) != 0) {
 
-			
-		char buf_end[4] = { '$','d','d','!' };
-
-		while (true) {
-
-			SQLHENV henv;
-			SQLHDBC hdbc;
-			SQLHSTMT hstmt;
-			SQLRETURN retcode;
-
-			SQLCHAR OutConnStr[255];
-			SQLSMALLINT OutConnStrLen;
-
-			HWND desktopHandle = GetDesktopWindow();   // desktop's window handle
-
-			// Allocate environment handle
-			retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
-
-			// Set the ODBC version environment attribute
-			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
-				retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
-
-				// Allocate connection handle
-				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
-					retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
-
-					// Set login timeout to 5 seconds
-					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
-						SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
-
-						retcode = SQLConnect(hdbc, (UCHAR*)"mssql", SQL_NTS, (UCHAR*)"sa", SQL_NTS, (UCHAR*)"123qwe", SQL_NTS);
-
-						// Allocate statement handle
-						if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
-							retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-
-							SQLINTEGER cbfname = SQL_NTS, cbfdata = SQL_NTS;
-
-							char name[5000] = "";
-							char data[5000] = "";
-
-							retcode = SQLExecDirectA(hstmt, (SQLCHAR*)"SELECT * FROM imageTable3 \WHERE number = (SELECT MAX(number)as number from imageTable3)", SQL_NTS);
-
-							std::ofstream fout;
-							fout.open("C:\\temp\\img02.jpg", std::ios::binary);
-
-							while ((retcode = SQLFetch(hstmt)) != SQL_NO_DATA)
-							{
-								while ((retcode = SQLGetData(hstmt, 1, SQL_C_CHAR, name, sizeof(name), &cbfname) != SQL_NO_DATA))
-								{
-									SQLINTEGER NumBytes = (cbfname > 5000) || (cbfname == SQL_NO_TOTAL) ? 5000 : cbfname;
-								}
-
-								while ((retcode = SQLGetData(hstmt, 2, SQL_C_BINARY, data, sizeof(data), &cbfdata)) != SQL_NO_DATA)
-								{
-									SQLINTEGER NumBytes = (cbfdata > 5000) || (cbfdata == SQL_NO_TOTAL) ? 5000 : cbfdata;
-
-									fout.write(data, 5000);
-								}
-							}
-							fout.close();
-
-							// Process data
-							if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
-								SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
-							}
-
-							SQLDisconnect(hdbc);
-						}
-
-						SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
-					}
-				}
-				SQLFreeHandle(SQL_HANDLE_ENV, henv);
-			}
-
-			try
-			{
-				file.hFile = file.loadFile("C:\\temp\\img02.jpg");
-				file.setFileSize(GetFileSize(file.hFile, NULL));
-				file.hFileMapping = file.mappingFile();
-				std::cout << "시작 : " << std::endl;
-				//Token '$','^','@'
-				char buf_start[6] = { '#','i','$','d','d','!' };
-				printf("%c\n", buf_start[5]);
-				printf("%d\n", buf_start[5]);
-				//Image Size byte로 변환 
-				char c_size[100];
-				std::string str_size = std::to_string(file.getFileSize());
-				str_size.append("$dd!");
-				std::strncpy(c_size, str_size.c_str(), str_size.length());
-
-				SendMsg(buf_start, sizeof(buf_start)); //이미지 시작 Flag 전송
-				SendMsg(c_size, str_size.length()); //이미지 사이즈 전송
-
-				std::cout << "파일 사이즈 전송 : " << std::endl;
-				std::cout << "파일사이즈 : " << file.getFileSize() << std::endl;
-
-
-				SOCKADDR_IN dataAddr;
-				int dataAddrSz = sizeof(dataAddr);
-				int retval;
-
-				DWORD dwFileSizeHigh;
-				__int64 qwFileSize = GetFileSize(file.hFile, &dwFileSizeHigh);
-				__int64 qwFileOffset = 0;
-
-				WaitForSingleObject(hMutex, INFINITE);
-				while (qwFileSize > 0) {
-					DWORD dwBytesInBlock = BUFSIZE;
-					if (qwFileSize < BUFSIZE) {
-						dwBytesInBlock = qwFileSize;
-					}
-
-					file.pbFile = (char*)MapViewOfFile
-					(
-						file.hFileMapping,
-						FILE_MAP_READ,
-						(DWORD)(qwFileOffset >> 32),  // 상위 오프셋
-						(DWORD)(qwFileOffset & 0xFFFFFFFF),
-						dwBytesInBlock
-					);
-
-					retval = send(hClntSock, file.pbFile, dwBytesInBlock, 0);
-					if (retval != dwBytesInBlock) {
-						while (1) {
-							retval = send(hClntSock, file.pbFile, dwBytesInBlock, 0);
-							if (retval != 0) break;
-						}
-					}
-
-					// 뷰를 다 썼으므로, 뷰를 해제한다.
-					UnmapViewOfFile(file.pbFile);
-
-					// 오프셋 및 남은 파일 크기 갱신
-					qwFileOffset += dwBytesInBlock;
-					qwFileSize -= dwBytesInBlock;
-				}
-				ReleaseMutex(hMutex);
-				SendMsg(buf_end, sizeof(buf_end)); //이미지 END Flag 전송
-				std::cout << "파일전송 완료 : " << std::endl;
-				CloseHandle(file.hFile);
-				CloseHandle(file.hFileMapping);
-				Sleep(500);
-			}
-			catch (const std::exception&)
-			{
-				std::cout << "File IO Error" << std::endl;
-			}
-
-		}
 	}
 
 	WaitForSingleObject(hMutex, INFINITE);
@@ -305,3 +167,172 @@ void SendMsg(char * msg, int len)
 	ReleaseMutex(hMutex);
 }
 
+
+unsigned WINAPI SendImage(void *arg)
+{
+	char buf_end[4] = { '$','d','d','!' };
+	////////////////////////
+	///////////////////////
+	int i;
+	while (true)
+	{
+		if (!(clntCnt <= 0))
+		{
+			WaitForSingleObject(hMutex, INFINITE);
+			for (i = 0; i < clntCnt; i++) {
+
+				SQLHENV henv;
+				SQLHDBC hdbc;
+				SQLHSTMT hstmt;
+				SQLRETURN retcode;
+
+				SQLCHAR OutConnStr[255];
+				SQLSMALLINT OutConnStrLen;
+
+				HWND desktopHandle = GetDesktopWindow();   // desktop's window handle
+
+				// Allocate environment handle
+				retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+				// Set the ODBC version environment attribute
+				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+					retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
+
+					// Allocate connection handle
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+						// Set login timeout to 5 seconds
+						if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+							SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+							retcode = SQLConnect(hdbc, (UCHAR*)"mssql", SQL_NTS, (UCHAR*)"sa", SQL_NTS, (UCHAR*)"123qwe", SQL_NTS);
+
+							// Allocate statement handle
+							if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+								retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+
+								SQLINTEGER cbfname = SQL_NTS, cbfdata = SQL_NTS;
+
+								char name[5000] = "";
+								char data[5000] = "";
+
+								retcode = SQLExecDirectA(hstmt, (SQLCHAR*)"SELECT * FROM imageTable3 \WHERE number = (SELECT MAX(number)as number from imageTable3)", SQL_NTS);
+
+								std::ofstream fout;
+								fout.open("C:\\temp\\img02.jpg", std::ios::binary);
+
+								while ((retcode = SQLFetch(hstmt)) != SQL_NO_DATA)
+								{
+									while ((retcode = SQLGetData(hstmt, 1, SQL_C_CHAR, name, sizeof(name), &cbfname) != SQL_NO_DATA))
+									{
+										SQLINTEGER NumBytes = (cbfname > 5000) || (cbfname == SQL_NO_TOTAL) ? 5000 : cbfname;
+									}
+
+									while ((retcode = SQLGetData(hstmt, 2, SQL_C_BINARY, data, sizeof(data), &cbfdata)) != SQL_NO_DATA)
+									{
+										SQLINTEGER NumBytes = (cbfdata > 5000) || (cbfdata == SQL_NO_TOTAL) ? 5000 : cbfdata;
+
+										fout.write(data, 5000);
+									}
+								}
+								fout.close();
+
+								// Process data
+								if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+									SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+								}
+
+								SQLDisconnect(hdbc);
+							}
+
+							SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+						}
+					}
+					SQLFreeHandle(SQL_HANDLE_ENV, henv);
+				}
+
+				try
+				{
+					file.hFile = file.loadFile("C:\\temp\\img02.jpg");
+					file.setFileSize(GetFileSize(file.hFile, NULL));
+					file.hFileMapping = file.mappingFile();
+					std::cout << "시작 : " << std::endl;
+					//Token '$','^','@'
+					char buf_start[6] = { '#','i','$','d','d','!' };
+					//Image Size byte로 변환 
+					char c_size[100];
+					std::string str_size = std::to_string(file.getFileSize());
+					str_size.append("$dd!");
+					std::strncpy(c_size, str_size.c_str(), str_size.length());
+
+
+					send(clntSocks[i], buf_start, sizeof(buf_start), 0);//이미지 시작 Flag 전송
+					send(clntSocks[i], c_size, str_size.length(), 0); //이미지 사이즈 전송
+
+					std::cout << "파일 사이즈 전송 : " << std::endl;
+					std::cout << "파일사이즈 : " << file.getFileSize() << std::endl;
+
+
+					SOCKADDR_IN dataAddr;
+					int dataAddrSz = sizeof(dataAddr);
+					int retval;
+
+					DWORD dwFileSizeHigh;
+					__int64 qwFileSize = GetFileSize(file.hFile, &dwFileSizeHigh);
+					__int64 qwFileOffset = 0;
+
+					WaitForSingleObject(hMutex, INFINITE);
+					while (qwFileSize > 0) {
+						DWORD dwBytesInBlock = BUFSIZE;
+						if (qwFileSize < BUFSIZE) {
+							dwBytesInBlock = qwFileSize;
+						}
+
+						file.pbFile = (char*)MapViewOfFile
+						(
+							file.hFileMapping,
+							FILE_MAP_READ,
+							(DWORD)(qwFileOffset >> 32),  // 상위 오프셋
+							(DWORD)(qwFileOffset & 0xFFFFFFFF),
+							dwBytesInBlock
+						);
+
+						retval = send(clntSocks[i], file.pbFile, dwBytesInBlock, 0);
+						if (retval != dwBytesInBlock) {
+							while (1) {
+								retval = send(clntSocks[i], file.pbFile, dwBytesInBlock, 0);
+								if (retval != 0) break;
+							}
+						}
+
+						// 뷰를 다 썼으므로, 뷰를 해제한다.
+						UnmapViewOfFile(file.pbFile);
+
+						// 오프셋 및 남은 파일 크기 갱신
+						qwFileOffset += dwBytesInBlock;
+						qwFileSize -= dwBytesInBlock;
+					}
+					ReleaseMutex(hMutex);
+
+					send(clntSocks[i], buf_end, sizeof(buf_end), 0);//이미지 시작 Flag 전송
+					//SendMsg(buf_end, sizeof(buf_end)); //이미지 END Flag 전송
+					std::cout << "파일전송 완료 : " << std::endl;
+					CloseHandle(file.hFile);
+					CloseHandle(file.hFileMapping);
+				}
+				catch (const std::exception&)
+				{
+					std::cout << "File IO Error" << std::endl;
+				}
+
+
+
+			}
+			ReleaseMutex(hMutex);
+		}
+		Sleep(500);
+	}
+
+	return 0;
+}
